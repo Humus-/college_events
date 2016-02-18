a404aa6f6a5d57f50cdb9954e2c0a7bce112df30
Diptanshu Kakwani <dipkakwani@gmail.com>
2016-02-19 00:30:07 +0530

    Add google sign-in button

---
+++ app/evento.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run()
